c16b9f553ea2290efb550df8a7b09b37223027fb
2pshcd4u <natec10tech@gmail.com>
2022-08-25 10:12:51 -0400

    commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./bootstrap.min.css";
-import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
